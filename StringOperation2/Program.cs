a06c8e8cdddcc28ded943b991758763e14a026c2
meghanakl <meghanaraokl@gmail.com>
2019-07-05 01:23:27 +0530

    commit my project

---
+++ StringOperation2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stringreverse();
+    }
+    public static void Stringreverse()
+    {
+
+        string Str, reversestring = "";
+        int Length;
+        Console.Write("Enter A String : ");
+        Str = Console.ReadLine();
+        Length = Str.Length - 1;
+        if (Length >= 5)
+            Console.WriteLine(" string is bad");
+        else if (Length <= 5)
+            Console.WriteLine(" String is good");
+
+        Console.ReadLine();
+    }
+}
+
+
+
+
