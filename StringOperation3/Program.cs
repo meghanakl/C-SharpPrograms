e6386cbacccc54c3bc5a1418267928a8326cd6ed
meghanakl <meghanaraokl@gmail.com>
2019-07-05 11:17:36 +0530

    commit StringOperation3

---
+++ StringOperation3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stringreverse();
+    }
+    public static void Stringreverse()
+    {
+
+        string Str = "";
+        int Length;
+        Console.Write("Enter A String : ");
+        Str = Console.ReadLine();
+        Length = Str.Length;
+        if (Length == 5)
+        {
+            Console.WriteLine(" string is average");
+        }
+        else if (Length < 5)
+        {
+            Console.WriteLine(" string is bad");
+        }
+        else
+        {
+            Console.WriteLine(" String is good");
+        }
+        if (Str.Contains("a"))
+        {
+            Console.WriteLine(" String contain a");
+        }
+        else
+        {
+            Console.WriteLine(" String  does not contain a");
+        }
+
+        Console.ReadLine();
+
+    }
+}
+s
